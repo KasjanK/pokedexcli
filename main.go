6e1eaa9a3d141166130874652ba031d02b9a35df
KasjanK <kowalczykkasjan@gmail.com>
2025-10-08 08:17:10 +0200

    feat: command system, execute code based on command

--- main.go
+++ main.go
@@ -7,15 +7,70 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name 			string
+	description 	string
+	callback func() error
+}
+
+func supportedCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		input := strings.Fields(scanner.Text())
-		fmt.Println("Your command was:", strings.ToLower(input[0]))
 
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		
+		commandName := input[0]
+		
+		command, ok := supportedCommands()[commandName]
+		if ok { 
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for comm, desc := range supportedCommands() {
+		fmt.Println(comm, desc.description)
+	}
+	fmt.Println()
+	return nil
 }
 
 func cleanInput(text string) []string {
